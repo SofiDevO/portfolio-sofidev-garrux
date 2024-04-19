f4a947370c0a359ee0afe69a46c899117a9c3b25
SofiDevO <angiemeow89@gmail.com>
2024-04-18 23:37:48 -0600

    :sparkles: Feat: Se agrega nuevo estilo a las cards de Portafolio

---
+++ src/components/data/portfolioData.js
@@ -0,0 +1,40 @@
+export const portafolioData = [
+	{
+		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
+		titulo: 'Restaurant',
+		skills: ['React', 'StyledComponents'],
+		descripcion: 'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+		demoURL: '',
+		repoURL: '',
+		anim: 'fade-right',
+	},
+	{
+		imgSrc: 'https://themewagon.com/wp-content/uploads/2020/12/eflyer.jpg',
+		titulo: 'E-commerce',
+		skills: ['JavaScript', 'Bootstrap'],
+		descripcion: ' Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+		demoURL: '',
+		repoURL: '',
+		anim: 'fade-up',
+	},
+	{
+		imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
+		titulo: 'Coffe Store',
+		skills: ['React', 'CSS'],
+		descripcion: 'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+		demoURL: '',
+		repoURL: '',
+		anim: 'fade-left',
+	},
+];
+
+export const skillIcons = {
+	JavaScript: 'skill-icons:javascript',
+	React: 'skill-icons:react-dark',
+	Astro: 'skill-icons:astro',
+	CSS: 'skill-icons:css',
+	Sass: 'skill-icons:sass',
+	StyledComponents: 'skill-icons:styledcomponents',
+	Bootstrap: 'skill-icons:bootstrap',
+	Tailwind: 'skill-icons:tailwindcss-dark',
+};
