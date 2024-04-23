45569f058d701e61754516c35db9143f3a1a44b5
SofiDev <angiemeow89@gmail.com>
2024-04-22 23:37:39 -0600

    ✨ Feat:menu responsive
    Se agrega menu versión mobile

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,7 +1,8 @@
 export const mainMenu = [
-     {link:"#potafolio", content:"Portafolio"},
-     {link:"#", content:"About"},
-     {link:"#", content:"Blog"}
+     {link:"#", content:"Home", icon:"lucide:home"},
+     {link:"#potafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"#about", content:"About", icon:"octicon:person-24"},
+     {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
 export const menuBlog = [
      {link:"#", content:"Portafolio"},
