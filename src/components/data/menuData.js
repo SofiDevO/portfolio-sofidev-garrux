4d4830c65d2caeebf074e01f1ba3ea8d284d51bc
SofiDev <angiemeow89@gmail.com>
2024-04-27 23:19:57 -0600

    ✨ Feat: pagina de Blog
    Se dan a estilos a la página de blog

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,11 +1,11 @@
 export const mainMenu = [
      {link:"#", content:"Home", icon:"lucide:home"},
      {link:"#about", content:"About", icon:"octicon:person-24"},
-     {link:"#potafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
      {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
 export const menuBlog = [
      {link:"/", content:"Home"},
-     {link:"#", content:"About"},
-     {link:"#", content:"Portfolio"}
+     {link:"/#about", content:"About"},
+     {link:"/#portafolio", content:"Portfolio"}
 ]
